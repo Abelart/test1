a37bfaef45013b60b1f93749ade5a5f8c1038d21
Abelart <abel.galiano.molina@gmail.com>
2020-10-05 19:33:51 +0200

    Third commit
    Tercer commit

---
+++ Prueba/Prueba/Geometry.cs
@@ -0,0 +1,27 @@
+﻿namespace Ecuaciones
+{
+    class Geometry
+    {
+        public static double GetVector2DLength(double x, double y)
+        {
+            double aux = x * x + y * y;
+            double lenght = System.Math.Sqrt(aux);
+            return lenght;
+        }
+
+        public static double GetVector3DLength(double x, double y, double z)
+        {
+            double aux = x * x + y * y + z * z;
+            double lenght = System.Math.Sqrt(aux);
+            return lenght;
+
+        }
+
+        public static double GetCircleArea(double r);
+        {
+            double aux = r * r;
+            double aux2 = System.Math.PI * (aux);
+           
+        }
+    }
+}
---
+++ Prueba/Prueba/Maths.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecuaciones
+{
+    class Maths
+    {
+        public static Equation2solution SolveEquation(double a, double b, double c)
+        {
+            /*
+            double aux1, aux2;
+            Equation2solution result;
+
+            result = new Equation2solution();
+
+            aux1 = b * b - 4 * a * c;
+            aux2 = System.Math.Sqrt(aux1);
+
+            result.solution1 = (-b + aux2) / (2 * a);
+            result.solution2 = (-b - aux2) / (2 * a);
+            return result;
+            */
+            return null;
+
+        }
+
+        public static double SolveEquation2(double a, double b)
+        {
+            double x;
+            x = -b / a;     
+            return x;
+
+        }
+
+    }
+}
